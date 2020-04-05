4f37beca8b93182d0eb256e137f6ec90fd7ef95d
Roberto Chirila <robertochirila@yahoo.com>
2020-04-05 11:33:48 +0100

    added logic for the form user component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import { UserForm } from "./components/UserForm"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <UserForm/>
     </div>
   );
 }
